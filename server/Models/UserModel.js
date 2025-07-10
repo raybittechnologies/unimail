d420ff67e094cb2328e5e204372dc3e41381a65b
zahid-01 <erzahidkan101@gmail.com>
2025-07-10 14:37:02 +0530

    Push

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -60,7 +60,7 @@ module.exports = (sequelize, DataTypes) => {
     // 👇 Add the foreign key field
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: "Users", // Must match the table name (case-sensitive)
         key: "id",
