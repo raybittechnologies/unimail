6492021cf867513889850310ff89372b943fca2d
zahid-01 <erzahidkan101@gmail.com>
2025-07-16 22:24:08 +0530

    Push code to deploy

--- src/network/baseurl.js
+++ src/network/baseurl.js
@@ -1,2 +1,2 @@
-// export const BASE_URI = "https://beunimail.raybitprojects.com";
-export const BASE_URI = "http://localhost:5055";
+export const BASE_URI = "https://beunimail.raybitprojects.com";
+// export const BASE_URI = "http://localhost:5055";
